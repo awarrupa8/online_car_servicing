7a688eb50415b32fda6e33eb8d1b904be8d92e96
awarrupa8 <awarrupa8@gmail.com>
2021-03-03 17:16:43 +0530

    commit me

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Text>Hello  i am there </Text>
+      <Text>Hello  i am online car serviving </Text>
       <StatusBar style="auto" />
     </View>
   );
