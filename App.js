8add1515cfdc49e74e02d79e2e5fd7eb87ba3d0e
awarrupa8 <awarrupa8@gmail.com>
2021-03-03 18:02:34 +0530

    mmmm

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { Navigation } from 'react-native-navigation';
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Text>Hello  i am online car serviving </Text>
+      <Text>online car serviving </Text>
       <StatusBar style="auto" />
     </View>
   );
