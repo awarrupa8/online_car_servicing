397ddc5f734ad87a1bb5b84f683cda3803898387
awarrupa8 <awarrupa8@gmail.com>
2021-03-03 17:08:15 +0530

    m

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Text>Hello  i am there </Text>
+      <Text>Hello  i am rupa </Text>
       <StatusBar style="auto" />
     </View>
   );
